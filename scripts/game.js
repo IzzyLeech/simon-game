8bf0650e8a4cfa34e2b4835dd3c3304ee74e8ba1
IzzyLeech <ralphhunter46@gmail.com>
2023-01-28 18:18:44 +0000

    add turnNumber

--- scripts/game.js
+++ scripts/game.js
@@ -2,6 +2,7 @@ let game = {
     score: 0,
     currentGame: [],
     playerMoves: [],
+    turnNumber: 0,
     choices: ["button1", "button2", "button3", "button4"],
 }
 
@@ -16,7 +17,7 @@ function newGame() {
 function addTurn() {
     game.playerMoves = [];
     game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
-    //showTurns;
+    showTurns();
 }
 
 function showScore() {
@@ -25,9 +26,20 @@ function showScore() {
 
 function lightsOn(circ) {
     document.getElementById(circ).classList.add("light");
-    setTimeout(() => {
+    setTimeout( () => {
         document.getElementById(cicr).classList.remove("light");
     }, 400);
 }
 
-module.exports = {game, newGame, showScore, addTurn, lightsOn};
+function showTurns() {
+    game.turnNumber = 0;
+    let turns = setInterval(() => {
+        lightsOn(game.currentGame[game.turnNumber]);
+        game.turnNumber++;
+        if (game.turnNumber >= game.currentGame.lenght) {
+            clearInterval(turns);
+        }
+    }, 800)
+}
+
+module.exports = {game, newGame, showScore, addTurn, lightsOn, showTurns};
